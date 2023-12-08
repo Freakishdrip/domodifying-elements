0e3ab9eabd67deb6a2467f9bdb0a56805cc0c16f
Nick Drummond <nckdrmmnd@gmail.com>
2023-12-08 16:36:40 -0700

    somehow broken

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.setAttribute('id','victory');
+document.body.append(newHeader);
+document.getElementsByTagName("h1")[0].innerHTML="Nicholas Drummond is the champion";
